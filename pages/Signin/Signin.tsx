61b121612a498571e29bba1e5414eaf75964bcf0
SinChaNo <atom7667@naver.com>
2021-12-17 04:03:29 +0900

    postion center

--- pages/Signin/Signin.tsx
+++ pages/Signin/Signin.tsx
@@ -2,9 +2,9 @@ import styles from "./styles/signin.module.scss"
 
 const Signin = () => {
   return (
-    <form className = {`mx-auto ${styles.loginform}`}>
+    <form className = {`mx-auto position-absolute top-50 start-50 translate-middle ${styles.loginform}`}>
       <div className="mb-3">
-        <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
+        <label htmlFor="exampleInputEmail1" className="form-label">Email</label>
         <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
         <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
       </div>
@@ -12,10 +12,7 @@ const Signin = () => {
         <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
         <input type="password" className="form-control" id="exampleInputPassword1" />
       </div>
-      <div className="mb-3 form-check">
-        <input type="checkbox" className="form-check-input" id="exampleCheck1" />
-        <label className="form-check-label" htmlFor="exampleCheck1">Check me out</label>
-      </div>
+
       <button type="submit" className="btn btn-primary">Submit</button>
     </form>
   )
